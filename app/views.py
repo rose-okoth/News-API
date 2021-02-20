a4a22f226463e808c771cae849244dae3883e35d
rose-okoth <okoth.rose0@gmail.com>
2021-02-20 12:24:40 +0300

    added in the news html file

--- app/views.py
+++ app/views.py
@@ -7,4 +7,15 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    message = 'Latest News'
+    return render_template('index.html',message =  message)
+    
+
+@app.route('/')
+def news():
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html')
+
