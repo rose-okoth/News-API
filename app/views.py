6b7e84d7625131d78fa54cf7309ad5d3095db8b0
rose-okoth <okoth.rose0@gmail.com>
2021-02-21 00:16:15 +0300

    debugged the view.py and html files

--- app/views.py
+++ app/views.py
@@ -1,26 +1,40 @@
 from flask import render_template 
 from app import app
-from .request import get_news,process_results
-
-
+from .request import get_news, process_results
+from config import news_api_base_url,api_key
+import urllib.request,json
 #views 
 @app.route('/')
 def index():
-    '''
-    View root page function that returns the index page and its data
-    '''
-    all_news = get_news('everything')
-    print(all_news)
-    title = 'Welcome to the home of the Latest News'
-
-    my_items = zip(source,author,title,description,url,publishedAt,urlToImage,anything)
-    return render_template('index.html',title = title,everything = my_items)
-
-
-@app.route('/news')
-def news():
-    '''
-    View news page function that returns the news details page and its data
-    '''
-    return render_template('news.html')
-
+    get_news_url = news_api_base_url.format(api_key)
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+        news_results = None 
+        if get_news_response['articles']:
+            news_results_list = get_news_response['articles']
+            news_results = process_results(news_results_list)
+    return news_results
+def process_results(news_list):
+    #Function that processes the news results
+    source = []
+    author = []
+    title = []
+    description = []
+    url = []
+    publishedAt = []
+    urlToImage = []
+    anything = []
+    news_results = []
+    for news_item in news_list:
+        name = news_item.get('source')
+        source.append(name.get('name'))
+        author.append(news_item.get('author'))
+        title.append(news_item.get('title'))
+        description.append(news_item.get('description'))
+        url.append(news_item.get('url'))
+        publishedAt.append(news_item.get('publishedAt'))
+        urlToImage.append(news_item.get('urlToImage'))
+        anything.append('dsds')
+    news_results=zip(source,author,title,description,url,publishedAt,urlToImage,anything)
+    return render_template("index.html", content = news_results)
